2f3ab3e668ef9e8ac746ad6fe9986af39b7d15c9
iagochaves-encoura <iago.chaves@encoura.org>
2023-08-01 18:19:05 -0300

    fix: deploy

--- sources/video-showcase/local-deploy.sh
+++ sources/video-showcase/local-deploy.sh
@@ -7,19 +7,12 @@ npm install
 echo "Running react build"
 npm run build
  
-echo "Deleting prior build"
-rm -rf "$SANDBOX_PATH"/static-assets/app/*
-
 echo "Copying app build to static assets"
-cp -r dist/* "$SANDBOX_PATH"/static-assets/app/
-
-echo "Delete react build output directory"
-rm -rf ./dist
+cp -r ../../static-assets/app/* "$SANDBOX_PATH"/static-assets/app/
 
 pushd "$SANDBOX_PATH" || exit
 git add static-assets/app && git commit -m "chore: Deploying new version of the app"
 popd || exit
-
 
 curl http://localhost:8080/api/1/site/context/rebuild?token=defaultManagementToken
 echo "\n Build finished"
